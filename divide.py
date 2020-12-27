3a95eb413e6f005d2cffe83e806bd540b8ec04ab
Krzysztof Wisniewski <cychug@gmail.com>
2020-12-27 18:24:16 +0100

    first commit

---
+++ divide.py
@@ -0,0 +1,17 @@
+
+
+def replaceString(text):
+    newText1 = text.replace("\"", "")
+    newText2 = newText1.replace(" ", ",", 1)
+    newText3 = newText2.replace(" - ", "-")
+    newText4 = newText3.replace(" – ", "-")
+    return newText4
+
+
+with open("input.csv") as inFile:
+    for line in inFile:
+        line = line.strip()
+        print(replaceString(line))
+        with open("output.csv", 'a') as outputFile:
+            outputFile.write(replaceString(line))
+            outputFile.write("\n")
